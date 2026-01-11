1665225cefa13b6e4b99a7499003d9cd77a786fc
harrisovcov1 <harrisovcov@gmail.com>
2026-01-11 13:02:25 +0000

    Delete sql/migrations directory

--- sql/migrations/005_starter_offer_fields.sql
+++
@@ -1,7 +0,0 @@
--- 005_starter_offer_fields.sql
--- Adds starter offer window + purchase flag to users.
--- Run this in Supabase SQL editor (safe to run multiple times).
-
-ALTER TABLE public.users
-  ADD COLUMN IF NOT EXISTS starter_offer_expires_at timestamptz,
-  ADD COLUMN IF NOT EXISTS starter_offer_purchased boolean NOT NULL DEFAULT FALSE;
